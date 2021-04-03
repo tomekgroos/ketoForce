0b89e93d18bc1a07c4ceb1bc3b2784e61f2a7821
Your Name <tomek.groos@gmail.com>
2021-04-03 18:06:12 +0200

    improved RWD

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 
 const upButton = document.querySelector(".up-button");
+const moveRight = document.querySelectorAll(".move-right");
+const moveLeft = document.querySelectorAll(".move-left");
 
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.animation = "fadeRight 1.5s ease-in";
+      observer.unobserve(entry.target);
+    } else if (entry.isIntersecting === true) {
+      return;
+    }
+  });
+}, options);
 
 const goUp = () => {
   window.addEventListener("scroll", () => {
