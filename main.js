4aef7393cf2d50a677515dae370f12be6dc19d22
Your Name <tomek.groos@gmail.com>
2021-04-04 19:37:48 +0200

    end of day 04.04.2021

--- main.js
+++ main.js
@@ -1,19 +1,51 @@
 
+// grabbed elements
+
 const upButton = document.querySelector(".up-button");
 const moveRight = document.querySelectorAll(".move-right");
 const moveLeft = document.querySelectorAll(".move-left");
 
 
-const observer = new IntersectionObserver((entries) => {
+// animation fade left and right
+
+const options = {
+  root: null,
+  threshold: 0.1,
+  rootMargin: "50px",
+};
+
+const observerRight = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.intersectionRatio > 0) {
-      entry.target.style.animation = "fadeRight 1.5s ease-in";
-      observer.unobserve(entry.target);
+      entry.target.style.animation = "fadeRight 1.5s ease-in-out";
+      observerRight.unobserve(entry.target);
     } else if (entry.isIntersecting === true) {
       return;
     }
   });
 }, options);
+
+const observerLeft = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.style.animation = "fadeLeft 1.5s ease-in-out";
+      observerLeft.unobserve(entry.target);
+    } else if (entry.isIntersecting === true) {
+      return;
+    }
+  });
+}, options);
+
+moveRight.forEach((el) => {
+  observerRight.observe(el);
+});
+
+moveLeft.forEach((el) => {
+  observerLeft.observe(el);
+});
+
+
+// back to top button
 
 const goUp = () => {
   window.addEventListener("scroll", () => {
